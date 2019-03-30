fd9c7db2eab3b677255e2f46a92ac486a41dfc15
James Reed <jcrd@tuta.io>
2019-03-30 17:33:40 -0600

    test: Fix printing received ID

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ echo "[TEST] launching $CMD ($ID)"
 (sleep 1; LD_PRELOAD=../wm-launch-preload.so WM_LAUNCH_ID=$ID $CMD > "$ID".log 2>&1) &
 
 r="$(./maprequestwait 2>&1)"
-echo "[TEST] map request received ($ID)"
+echo "[TEST] map request received ($r)"
 
 if [ "$r" = "$ID" ]; then
     echo "[TEST] SUCCESS"
